f106b06da984b753516b41f3946c0cefb4828295
anonymous <anonymous@anonymous.org>
2020-02-03 00:42:33 +0800

    active background page on popup show

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const billTypeSelect = document.getElementById('billTypeSelect');
     const payeeNameDiv = document.getElementById('payeeNameDiv');
     const payeeNameInput = document.getElementById('payeeNameInput');
@@ -14,87 +15,91 @@ document.addEventListener('DOMContentLoaded', function() {
     const totalPaidInput = document.getElementById('totalPaidInput');
     const resetButton = document.getElementById('resetButton');
 
-    function updateUi() {
-        const result = chrome.extension.getBackgroundPage().queryStatus();
-        billTypeSelect.value = result.params.billType;
-        payeeNameInput.value = result.params.payeeName;
-        taxAccountNumberInput.value = result.params.taxAccountNumber;
-        payAccountInput.value = result.params.payAccount;
-        payAmountInput.value = result.params.payAmount;
-        payCountInput.value = result.params.payCount;
-        payeeNameDiv.style.display = billTypeSelect.value == 'REGISTERED_PAYEE' ? '' : 'none';
-        taxAccountNumberDiv.style.display = billTypeSelect.value == 'TAX' ? '' : 'none';
+    chrome.runtime.getBackgroundPage(function(backgroundPage){
 
-        const runningPayLoop = result.currentPayLoop && !result.currentPayLoop.stopped;
-        billTypeSelect.disabled = runningPayLoop;
-        payeeNameInput.disabled = runningPayLoop;
-        taxAccountNumberInput.disabled = runningPayLoop;
-        payAccountInput.disabled = runningPayLoop;
-        payAmountInput.disabled = runningPayLoop;
-        payCountInput.disabled = runningPayLoop;
-        startButton.disabled = runningPayLoop;
-        stopButton.disabled = !result.currentPayLoop || (result.currentPayLoop.stopping || result.currentPayLoop.stopped);
-        
-        payProgress.max = result.currentPayLoop ? result.currentPayLoop.payCount : 0;
-        payProgress.value = result.currentPayLoop ? result.currentPayLoop.paymentMade : 0;
-        if (result.currentPayLoop && result.currentPayLoop.errorMessage) {
-            errorText.innerText = result.currentPayLoop.errorMessage;
-            errorText.style.display = '';
-        } else {
-            errorText.innerText = '';
-            errorText.style.display = 'none';
+        function updateUi() {
+            const result = backgroundPage.queryStatus();
+            billTypeSelect.value = result.params.billType;
+            payeeNameInput.value = result.params.payeeName;
+            taxAccountNumberInput.value = result.params.taxAccountNumber;
+            payAccountInput.value = result.params.payAccount;
+            payAmountInput.value = result.params.payAmount;
+            payCountInput.value = result.params.payCount;
+            payeeNameDiv.style.display = billTypeSelect.value == 'REGISTERED_PAYEE' ? '' : 'none';
+            taxAccountNumberDiv.style.display = billTypeSelect.value == 'TAX' ? '' : 'none';
+
+            const runningPayLoop = result.currentPayLoop && !result.currentPayLoop.stopped;
+            billTypeSelect.disabled = runningPayLoop;
+            payeeNameInput.disabled = runningPayLoop;
+            taxAccountNumberInput.disabled = runningPayLoop;
+            payAccountInput.disabled = runningPayLoop;
+            payAmountInput.disabled = runningPayLoop;
+            payCountInput.disabled = runningPayLoop;
+            startButton.disabled = runningPayLoop;
+            stopButton.disabled = !result.currentPayLoop || (result.currentPayLoop.stopping || result.currentPayLoop.stopped);
+            
+            payProgress.max = result.currentPayLoop ? result.currentPayLoop.payCount : 0;
+            payProgress.value = result.currentPayLoop ? result.currentPayLoop.paymentMade : 0;
+            if (result.currentPayLoop && result.currentPayLoop.errorMessage) {
+                errorText.innerText = result.currentPayLoop.errorMessage;
+                errorText.style.display = '';
+            } else {
+                errorText.innerText = '';
+                errorText.style.display = 'none';
+            }
+            totalPaidInput.value = result.totalPaid.toFixed(2);
         }
-        totalPaidInput.value = result.totalPaid.toFixed(2);
-    }
 
-    updateUi();
+        updateUi();
 
-    chrome.runtime.onMessage.addListener(function(message, sender, callback) {
-        if (message.type == "update" || message.type == "completed" || message.type == "failed") {
+        chrome.runtime.onMessage.addListener(function(message, sender, callback) {
+            if (message.type == "update" || message.type == "completed" || message.type == "failed") {
+                updateUi();
+            }
+        });
+
+        billTypeSelect.addEventListener('change', (event) => {
+            backgroundPage.setParams({billType: event.target.value});
             updateUi();
-        }
-    });
+        });
+        payeeNameInput.addEventListener('change', (event) => {
+            backgroundPage.setParams({payeeName: event.target.value});
+        });
+        taxAccountNumberInput.addEventListener('change', (event) => {
+            backgroundPage.setParams({taxAccountNumber: event.target.value});
+        });
+        payAccountInput.addEventListener('change', (event) => {
+            backgroundPage.setParams({payAccount: event.target.value});
+        });
+        payAmountInput.addEventListener('change', (event) => {
+            backgroundPage.setParams({payAmount: event.target.value});
+        });
+        payCountInput.addEventListener('change', (event) => {
+            backgroundPage.setParams({payCount: event.target.value});
+        });
 
-    billTypeSelect.addEventListener('change', (event) => {
-        chrome.extension.getBackgroundPage().setParams({billType: event.target.value});
-        updateUi();
-    });
-    payeeNameInput.addEventListener('change', (event) => {
-        chrome.extension.getBackgroundPage().setParams({payeeName: event.target.value});
-    });
-    taxAccountNumberInput.addEventListener('change', (event) => {
-        chrome.extension.getBackgroundPage().setParams({taxAccountNumber: event.target.value});
-    });
-    payAccountInput.addEventListener('change', (event) => {
-        chrome.extension.getBackgroundPage().setParams({payAccount: event.target.value});
-    });
-    payAmountInput.addEventListener('change', (event) => {
-        chrome.extension.getBackgroundPage().setParams({payAmount: event.target.value});
-    });
-    payCountInput.addEventListener('change', (event) => {
-        chrome.extension.getBackgroundPage().setParams({payCount: event.target.value});
-    });
+        startButton.addEventListener('click', async function() {
+            event.preventDefault();
 
-    startButton.addEventListener('click', async function() {
-        event.preventDefault();
+            const payAmount = parseFloat(payAmountInput.value);
+            const payCount = parseInt(payCountInput.value);
+            backgroundPage.start({payAmount, payCount})
+        });
 
-        const payAmount = parseFloat(payAmountInput.value);
-        const payCount = parseInt(payCountInput.value);
-        chrome.extension.getBackgroundPage().start({payAmount, payCount})
+        stopButton.addEventListener('click', async function() {
+            event.preventDefault();
+
+            backgroundPage.stop();
+            updateUi();
+        });
+
+        resetButton.addEventListener('click', function() {
+            event.preventDefault();
+
+            backgroundPage.resetCounter();
+            updateUi();
+        });
+
     });
-
-    stopButton.addEventListener('click', async function() {
-        event.preventDefault();
-
-        chrome.extension.getBackgroundPage().stop();
-        updateUi();
-    });
-
-    resetButton.addEventListener('click', function() {
-        event.preventDefault();
-
-        chrome.extension.getBackgroundPage().resetCounter();
-        updateUi();
-    });
-
+    
 });
